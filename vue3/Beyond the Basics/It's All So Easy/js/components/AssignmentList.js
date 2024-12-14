72b1ea6a19d7b5480f812cc25ba64d285964f288
Imtiaze <ahammedimtiaze78@gmail.com>
2024-12-14 20:15:07 +0600

    Beyond the basics

--- vue3/Beyond the Basics/It's All So Easy/js/components/AssignmentList.js
+++ vue3/Beyond the Basics/It's All So Easy/js/components/AssignmentList.js
@@ -6,10 +6,25 @@ export default {
     
     template: `
         <section v-show="assignments.length">
-            <h2 class="mb-2 font-bold">{{ title }}</h2>
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <h2 class="mb-2 font-bold">
+                {{ title }}
+                <span>({{ assignments.length }})</span>
+            </h2>
+
+            <div class="flex gap-2">
+                <button 
+                    @click="currentTag = tag"
+                    v-for="tag in tags" 
+                    class="border rounded px-2 py-1 text-xs"
+                    :class="{
+                        'border-blue-500 text-blue-500': tag === currentTag
+                    }"
+                >{{ tag }}</button>
+            </div>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment
-                    v-for="assignment in assignments" 
+                    v-for="assignment in filteredAssignments" 
                     :key="assignment.id"
                     :assignment="assignment"
                 >
@@ -24,5 +39,27 @@ export default {
             default: []
         },
         title: String
+    },
+
+    data() {
+        return {
+            currentTag: 'all'
+        }
+    },
+
+    computed: {
+
+        filteredAssignments() {
+
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+
+        tags() {
+            return ['all', ...new Set( this.assignments.map(assignment => assignment.tag))]
+        }
     }
 }
--- vue3/Beyond the Basics/It's All So Easy/js/components/Assignments.js
+++ vue3/Beyond the Basics/It's All So Easy/js/components/Assignments.js
@@ -16,9 +16,9 @@ export default {
     data() {
         return {
             assignments: [
-                {id: 1, name: 'Finish Homework', 'completed': false},
-                {id: 2, name: 'Read chapter 4', 'completed': false},
-                {id: 3, name: 'Turn in home', 'completed': false},
+                {id: 1, name: 'Finish Homework', 'completed': false, tag: 'math'},
+                {id: 2, name: 'Read chapter 4', 'completed': false, tag: 'science'},
+                {id: 3, name: 'Turn in home', 'completed': false, tag: 'math'},
             ],
         }
     },
@@ -40,6 +40,7 @@ export default {
                 id: this.assignments.length,
                 name: name,
                 completed: false,
+                tag: 'Social'
             });
 
         }
